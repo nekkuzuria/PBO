cabc798c4f2f7ff43ecb69cc5f1ddecab01799c8
Amanda F <44936062+nekkuzuria@users.noreply.github.com>
2023-04-28 11:05:33 +0700

    Update Sales.java

--- Pegawai/src/Sales.java
+++ Pegawai/src/Sales.java
@@ -5,8 +5,8 @@ public class Sales extends Pegawai {
     private int bonus;
 
 //    Constructor
-    Sales(String nip, String nama){
-        super(nip, nama);
+    Sales(String nip, String nama, String jenisKelamin){
+        super(nip, nama, jenisKelamin);
     }
 
 //    Setter Getter
@@ -33,5 +33,29 @@ public class Sales extends Pegawai {
 
     public void setBonus(int bonus) {
         this.bonus = bonus;
+    }
+
+    @Override
+    public void setNip(int urutanMasuk){
+        this.setNip("S"+urutanMasuk);
+    }
+    @Override
+    public int getGaji() { return super.getGaji() + this.bonus; }
+
+    @Override
+    public int getThr() {
+        if(this.getJmlPenjualan()>=this.getTargetPenjualan()) {
+            return this.getGaji() * 2;
+        }
+        else{
+            return this.getGaji() ;
+        }
+    }
+
+    //    Method
+    @Override
+    public boolean checkNip(String nip){
+        if(nip.charAt(0) == 'S') return true;
+        else return false;
     }
 }
